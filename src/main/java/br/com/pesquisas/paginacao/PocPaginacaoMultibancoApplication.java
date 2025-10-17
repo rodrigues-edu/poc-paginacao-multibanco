1c7605b7f8a10a6f93a567a7f79910d88f0858ea
Eduardo Rodrigues <silva.rodriguesedu@icloud.com>
2025-10-17 18:51:28 -0300

    :tada: Project Created! Initial Commit to Create the Multi-Database Pagination POC Project

---
+++ src/main/java/br/com/pesquisas/paginacao/PocPaginacaoMultibancoApplication.java
@@ -0,0 +1,13 @@
+package br.com.pesquisas.paginacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocPaginacaoMultibancoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocPaginacaoMultibancoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/pesquisas/paginacao/PocPaginacaoMultibancoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pesquisas.paginacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocPaginacaoMultibancoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
